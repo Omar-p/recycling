265200b5f308a6dffe07cc867c6313a9b4d20349
Omar Shabaan <omar.plus.077@gmail.com>
2022-11-05 06:14:21 +0200

    initial commit

---
+++ src/main/java/eg/edu/tanta/recycling/RecyclingApplication.java
@@ -0,0 +1,13 @@
+package eg.edu.tanta.recycling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecyclingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecyclingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eg/edu/tanta/recycling/RecyclingApplicationTests.java
@@ -0,0 +1,13 @@
+package eg.edu.tanta.recycling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecyclingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
