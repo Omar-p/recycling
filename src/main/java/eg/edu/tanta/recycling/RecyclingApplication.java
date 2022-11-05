59822c2ddad0864ee51975a81235863695df9e42
Omar Shabaan <omar.plus.077@gmail.com>
2022-11-05 07:55:27 +0200

    add Jwt encoder & decoder

--- src/main/java/eg/edu/tanta/recycling/RecyclingApplication.java
+++ src/main/java/eg/edu/tanta/recycling/RecyclingApplication.java
@@ -1,9 +1,12 @@
 package eg.edu.tanta.recycling;
 
+import eg.edu.tanta.recycling.security.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class RecyclingApplication {
 
 	public static void main(String[] args) {
